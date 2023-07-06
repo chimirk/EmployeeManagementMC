31e42634dff4f1a0ee1968a3b6de4c22baccd043
15144 <15144@LAPTOP-9USGHEML>
2023-07-06 13:15:40 -0400

    Add project files.

---
+++ EmployeeManagementMC.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EmployeeManagementMC.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ EmployeeManagementMC.Repository/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace EmployeeManagementMC.Repository
+{
+    public class Class1
+    {
+
+    }
+}
